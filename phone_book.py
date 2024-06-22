30868a6d91f4aec8645754c63cf858ed7c05128d
Kira <kirnan_t@outlok.com>
2024-06-22 15:27:31 +0700

    console interface for input and output

--- phone_book.py
+++ phone_book.py
@@ -4,7 +4,7 @@
 #функция для добавления, измения и удаления контакта
 phone_book = {}
 def add_contact (last_name, first_name, middle_name, phone_number):
-    contact_id= len(phone_book)+1
+    contact_id = len(phone_book)+1
     phone_book[contact_id]={'last_name': last_name, 'first_name': first_name, 'middle_name': middle_name, 'phone_number': phone_number}
 def update_contact (contact_id, last_name, first_name, middle_name, phone_number):
     if contact_id in phone_book:
@@ -35,4 +35,43 @@ def import_phone_book (filename):
         lines = file.readlines()
         for line in lines:
             contact_id, last_name, first_name, middle_name, phone_number = line.strip().split(',')
-            phone_book [int (contact_id)] ={'last_name': last_name, 'first_name': first_name, 'middle_name': middle_name, 'phone_number': phone_number}
+            phone_book [int (contact_id)] = {'last_name': last_name, 'first_name': first_name, 'middle_name': middle_name, 'phone_number': phone_number}
+# консольный интерфейс для ввода и вывода
+while True:
+    print("1. добавить контакт")
+    print("2. обновить контакт")
+    print("3. удалить контакт")
+    print("4. найти контакт")
+    print("5. экспорт телефоного справочника")
+    print("6. импорт телефоного справочника")
+    print("7. выход")
+    choice = input (" введите на свой выбор: ")
+    if choice == '1':
+        last_name = input("введите фамилию: ")
+        first_name = input("введите имя: ")
+        middle_name = input("введите отчество: ")
+        phone_number = input("введите номер: ")
+        add_contact(last_name, first_name, middle_name, phone_number)
+    elif choice =='2':
+        contact_id = int (input(" введите ID для обновления: "))
+        last_name = input("введите фамилию: ")
+        first_name = input("введите имя: ")
+        middle_name = input("введите отчество: ")
+        phone_number = input("введите номе: ")
+        update_contact(contact_id, last_name, first_name, middle_name, phone_number)
+    elif choice == '3':
+        contact_id = int (input("введите ID для удаления контакта: "))
+        delete_contact(contact_id)
+    elif choice == '4':
+        search_key = input ("введите имя или фамилию для поиска: ")
+        search_contact(search_key)
+    elif choice == '5':
+        export_file = input ("введите имя файла для экспорта: ") 
+        export_phone_book (export_file)
+    elif choice == '6':
+        import_file = input ("введите имя файла для импорта: ")
+        import_phone_book (import_file)
+    elif choice == '7':
+        break
+    else:
+        print("неверный ввод")
